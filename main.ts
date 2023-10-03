5f1e7ac2693648cd07a225c0b2f45d7c0edac204
Raju Ghorai <amirajubolchi001@gmail.com>
2023-10-03 22:21:51 +0530

    added formatter

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
-import fastify from 'fastify';
+import fastify from "fastify";
 
 async function bootstrap() {
-    // Create Fastify app instance
-    const app = fastify({ logger: true });
+  // Create Fastify app instance
+  const app = fastify({ logger: true });
 
-    app.get("/", (req, res) => {
-        res.send("Hello")
-    })
+  // Docker health check
+  app.get("/health", (req, res) => {
+    res.send("[+] Health check successful...");
+  });
 
+  // Start the server
+  const port = process.env.PORT ?? 3000;
+  await app.listen({ port: +port });
 
-    // Start the server
-    const port = process.env.PORT ?? 3000
-    await app.listen({ port: +port });
-
-    // Boilerplate for handling signals to gracefully shutdown a server
-    ["SIGINT", "SIGTERM"].forEach((signal) => {
-        process.on(signal, async () => {
-            await app.close();
-            process.exit(0);
-        })
-    })
+  // Boilerplate for handling signals to gracefully shutdown a server
+  ["SIGINT", "SIGTERM"].forEach((signal) => {
+    process.on(signal, async () => {
+      await app.close();
+      process.exit(0);
+    });
+  });
 }
 
 // Start the server
