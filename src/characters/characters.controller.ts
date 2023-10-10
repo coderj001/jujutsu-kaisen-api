ac0c2f4b0c3b3cde74f6a468beff966a8b2a2bea
Raju Ghorai <raju.ghorai@everest.engineering>
2023-10-10 13:27:25 +0530

    👨🏻‍🌾 Added: Find by name function

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -4,11 +4,14 @@ import { CharactersService, IQuery } from "./characters.service";
 import { serializeStringToNumeric } from "../helper/serialize-string-numeric";
 
 export class CharactersController {
-  constructor(private readonly charactersService: CharactersService) { }
+  constructor(private readonly charactersService: CharactersService) {}
 
   // async findAll({ limit, name, offset, sort }: IQuerystring = {}) {
-  async findAll({ limit, offset }: IQuerystring = {}) {
+  async findAll({ limit, offset, name }: IQuerystring = {}) {
     // const isSorted = sort != undefined && sort != null && Boolean(sort);
+    if (name) {
+      return await this.charactersService.findByName(name);
+    }
     const query: IQuery = {
       offset: serializeStringToNumeric(offset),
       limit: serializeStringToNumeric(limit),
@@ -23,7 +26,6 @@ type IQuerystring = {
   name?: string;
   offset?: string;
   limit?: string;
-  sort?: string;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -34,10 +36,8 @@ export function charactersController(
 ) {
   const characters = new CharactersController(new CharactersService());
   app.get<{ Querystring: IQuerystring }>("/", async (req, res) => {
-    const { name, offset, limit, sort } = req.query;
-    console.log(name);
-    console.log(sort);
-    const all = await characters.findAll({ limit, offset });
+    const { name, offset, limit } = req.query;
+    const all = await characters.findAll({ limit, offset, name });
     res.code(200).send(all);
   });
   done();
--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -7,7 +7,8 @@ export type IQuery = {
 };
 
 export class CharactersService {
-  private readonly dataService: EnService = new EnService();
+  // private readonly dataService: EnService = new EnService();
+  constructor(private readonly dataService: EnService = new EnService()) { }
 
   async findAll({ offset, limit }: IQuery = {}): Promise<Character[]> {
     offset = offset ?? 0;
@@ -17,5 +18,72 @@ export class CharactersService {
     const limited = skip.slice(0, limit);
 
     return limited;
+  }
+
+  async findByName(name: string): Promise<Character[]> {
+    const normalizedName = name.split(" ").map((name) =>
+      name
+        .normalize("NFD")
+        .toLowerCase(),
+    );
+    const all = await this.findAll();
+
+    const normalizeNames = all.map(({ name }, id): [number, string] => [
+      id,
+      name
+        .normalize("NFD")
+        .toLowerCase(),
+    ]);
+
+    let exactlyMatchId: number = -1;
+    const scores = [];
+    const alternatives: number[] = [];
+
+    for (let i = 0; i < normalizeNames.length; i++) {
+      const [id, name] = normalizeNames[i];
+      let points = 0;
+
+      if (name.toLowerCase() === normalizedName.join(" ")) {
+        exactlyMatchId = id;
+        break;
+      }
+
+      const splittedNames = name.split(" ");
+
+      normalizedName.forEach((_name) => {
+        const includes = splittedNames.filter(
+          (__name) => __name === _name,
+        ).length;
+        if (includes) points++;
+      });
+
+      if (points > 0) {
+        scores.push({ points, id });
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      normalizeNames.forEach(([_, _name]) => {
+        const includes = splittedNames.filter((__name) =>
+          __name.match(_name),
+        ).length;
+        if (includes) alternatives.push(id);
+      });
+    }
+
+    const semiMatch = scores.map(({ id }) => all[id]);
+    const alternativeMatch = alternatives.map((alt) => all[alt]);
+
+    const find =
+      exactlyMatchId >= 0
+        ? [all[exactlyMatchId]]
+        : semiMatch.length
+          ? semiMatch
+          : alternativeMatch;
+
+    if (!find.length) {
+      return [];
+    }
+
+    return find;
   }
 }
